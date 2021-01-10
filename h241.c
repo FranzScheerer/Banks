d96c91b2e5e34d9d7a46ce90f26fa1c95578c2a3
Franz Scheerer <scheerer.software@gmail.com>
2021-01-10 14:32:33 +0100

    Create h241.c

--- h241.c
+++ h241.c
@@ -1,94 +1,104 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define SWAP(A,B) tmp = A; A = B; B = tmp;
+#include <stddef.h>
 
-int a = 0, i = 0, j = 0, w = 1, s[256];
-int tmp;
+#include <string.h>
 
-void update()
-{
-    i = (i + w) % 256;
-    j = s[(j + s[i]) % 256];
-    SWAP(s[i], s[j])
-}
-int output()
-{
-    update();
-    return s[j];
-}
+#define LOW(B) ((B) & 0xf)
 
-void absorb_nibble(int x)
-{
-    if (a == 241){ 
-       for (int v = 0; v < 256; v++) 
-           update();
-       w = (w + 2) % 256;
-       a = 0;
-    }
-    SWAP(s[a], s[x+240])
-    a++;
-}
+#define HIGH(B) ((B) >> 4)
 
-void absorb_byte(int b)
-{
-    absorb_nibble(b % 16);
-    absorb_nibble(b / 16);
-}
+typedef struct State_0_ {
 
+  unsigned char s[256];
+  unsigned char a;
+  unsigned char i;
+  unsigned char j;
+  unsigned char k;
+  unsigned char w;
+  unsigned char z;
+} State_0;
 
-void squeeze(char *out, size_t outlen)
-{
-   int v;
-   for (v = 0; v < 256; v++) 
-       update();
-   w = (w + 2) % 256;
-   a = 0;
-   for (v = 0; v < outlen; v++) 
-       out[v] = output();
-}
+static void initialize_state(State_0 *state) {
 
-int main(int argn, char *argv[]){
-  int c,ii;
-  char txt[] = "The quick brown fox jumps over the lazy dog";
-  FILE *fp;
-  unsigned char out[32];
-  
-  fprintf(stderr,"hash %s\n",txt);
-  a = i = j = 0;
-  w = 1;
-  for (int v = 0; v < 256; v++) 
-     s[v] = v;
-  c = 0;
-  while ( txt[c] ){
-     absorb_byte(txt[c++]);
+  for (int v = 0; v < 256; v++) {
+      state->s[v] = (unsigned char) v;
+      state->s[v] += 3;
   }
-  squeeze(out, 32);
-  for (int ii=0; ii<32; ii++)  
-     fprintf(stderr,"%x", out[ii]);
-  fprintf(stderr, "\n");
-  a = i = j = 0;
-  w = 1;
-  for (int v = 0; v < 256; v++) 
-     s[v] = v;
-  c = 0;
-
-  if (argn == 1){
-    fprintf(stderr,"Usage ./a.out <filename>\n" );
-    exit(0);
-  } else {
-    fp = fopen(argv[1],"rb");
-    if (!fp){
-      fprintf(stderr,"error reading file %s\n",argv[1] );
-      exit(0);
-    }
-    while ( (c = fgetc(fp)) != -1 ){
-        absorb_byte(c);
-    }
-    fclose(fp);
-  }
-  squeeze(out, 32);
-  for (int ii=0; ii<32; ii++)  
-     fprintf(stderr,"%x", out[ii]);
-  fprintf(stderr, "\n");
+  state->a = state->i = state->j = 
+  state->k = state->w = state->z = 3;
 }
+
+static void update(State_0 *state) {
+
+  unsigned char t;
+  unsigned char y;
+  state->i += state->w;
+  y = state->j + state->s[state->i];
+  state->j = state->k + state->s[y];
+  state->k = state->i + state->k + state->s[state->j];
+  t = state->s[state->i];
+  state->s[state->i] = state->s[state->j];
+  state->s[state->j] = t;
+}
+
+static unsigned char output(State_0 *state) {
+
+  const unsigned char y1 = state->z + state->k;
+  const unsigned char x1 = state->i + state->s[y1];
+  const unsigned char y2 = state->j + state->s[x1];
+  state->z = state->s[y2];
+  return state->z;
+}
+
+static void absorb_nibble(State_0 *state, const unsigned char x) {
+
+  unsigned char t;
+  if (state->a == 128) {
+    for(int v = 0; v < 512; v++){ 
+       update(state);
+    }
+    state->w += 2;
+    state->a = 0;
+  }
+ 
+  t = state->s[state->a];
+  state->s[state->a] = state->s[128 + x];
+  state->s[128 + x] = t;
+  state->a++;
+}
+
+static void absorb_byte(State_0 *state, const unsigned char b) {
+
+  state->j += b;
+  absorb_nibble(state, LOW(b));
+  absorb_nibble(state, HIGH(b));
+}
+
+int main(int argn, char **argv){
+
+int i,c,cc;
+char pw[] = "top secret";
+State_0 state;
+initialize_state(&state);
+i = 0;
+while (pw[i]){
+   absorb_byte(&state, pw[i++]);
+}
+while ((c = fgetc(stdin)) != -1){
+   cc = c ^ output(&state);
+   if (argn > 1)
+      absorb_byte(&state, cc);
+   else
+      absorb_byte(&state, c);
+   update(&state);
+   fprintf(stdout,"%c", cc);
+}
+for (i=0;i<32;i++){
+   update(&state);
+   fprintf(stderr, "%02X", output(&state));
+}
+fprintf(stderr, "\n");
+return 0;
+}
+
